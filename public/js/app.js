d28f3e000db0ca7d048446752f4c5c4aa1e55f73
ainu <ainu@ainu-dell.ajchambeaud.com>
2014-06-02 07:43:23 -0300

    estructura general

---
+++ public/js/app.js
@@ -0,0 +1,93 @@
+(function(){
+	var app = angular.module('demo', ['ngRoute']);
+
+	app.config(['$routeProvider',
+	  function($routeProvider) {
+		$routeProvider.
+		  when('/home', {
+		    templateUrl: '/templates/home.html',
+		    controller: 'HomeController'
+		  }).
+		  when('/misrecursos', {
+		    templateUrl: '/templates/misrecursos.html',
+		    controller: 'MisRecursosController'
+		  }).
+          when('/buscar', {
+		    templateUrl: '/templates/buscar.html',
+		    controller: 'BuscarController'
+		  }).
+          when('/perfil', {
+		    templateUrl: '/templates/perfil.html',
+		    controller: 'PerfilController'
+		  });
+	}]);
+
+
+	app.directive('menuPrincipal', ['$http', function(){
+		return {
+			restrict: 'E',
+			templateUrl: '/templates/nav.html',
+			controller: function($http, $scope, $timeout){
+				var nav = this;
+				$http.get('/status').success(function(data){
+					nav.status = data;
+				});
+			},
+			controllerAs: 'nav'
+		};
+	}]);
+
+	app.controller('HomeController', function(){});
+
+	app.controller('MisRecursosController', function(){});
+
+	app.controller('BuscarController', ['$http',function($http){
+		var busqueda = this;
+		busqueda.buscando = "";
+		$http.get('/temas').success(function(data){
+			temas = data.result;
+			for(var i in temas){
+				temas[i].children = [];
+				for(var j in temas){
+					if(temas[i].id == temas[j].id_padre){
+						var t = temas[j];
+						temas[i].children.push(t);
+					}
+				}
+			}
+			console.log(temas);
+			busqueda.temas = temas;
+		});
+
+		this.buscarJuegos = function(){
+			$http.get('/juegos').success(function(data){
+				console.log(data);
+				juegos = data.result.data;
+				busqueda.recursos = juegos;
+				busqueda.buscando = "Juegos";
+			});
+		};
+
+		this.buscarVideos = function(){
+			$http.get('/videos').success(function(data){
+				console.log(data);
+				videos = data.result.data;
+				busqueda.recursos = videos;
+				busqueda.buscando = "Videos";
+			});
+		};
+
+		this.buscarInfografias = function(){
+			$http.get('/infografias').success(function(data){
+				console.log(data);
+				infografias = data.result.data;
+				busqueda.recursos = infografias;
+				busqueda.buscando = "Infografias";
+			});
+		};
+
+	}]);
+
+	app.controller('PerfilController', function(){});
+
+})()
---
+++ server.js
@@ -0,0 +1,127 @@
+var
+  express = require("express"),
+  path = require("path"),
+  nedb = require('nedb'),
+  http = require('http');
+  databaseUrl = "db/items.db";
+
+
+var db = {
+  items: new nedb({ filename: databaseUrl, autoload: true })
+};
+
+var app = express();
+
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser()),
+  app.use(express.static(path.join(__dirname, 'public')));
+  //Se crea directorio para servir archivos JSON estaticos a fin de hacer pruebas sin conexión a la API
+  //Eliminar en versión final
+  app.use("/dummyJSON", express.static(__dirname + '/dummyJSON'));
+});
+
+app.get('/api', function (req, res) {
+  res.send('API is running');
+});
+
+app.get('/status', function (req, res) {
+	require('dns').resolve('www.google.com', function(err) {
+	  if (err)
+	     res.send('offline');
+	  else
+	     res.send('online');
+	});
+});
+
+app.get('/temas', function(req, res){
+	var options = {
+	  host: 'localhost',
+	  port: 3000,
+	  path: '/dummyJSON/recursos.temas.json',
+	  method: 'GET'
+	};
+	http.request(options, function(response) {
+		var responseString = '';
+
+		response.on('data', function(data) {
+			responseString += data;
+		});
+
+		response.on('end', function() {
+			console.log(responseString);
+			var responseObject = JSON.parse(responseString);
+			res.send(responseObject);
+		});
+	}).end();
+});
+
+app.get('/juegos', function(req, res){
+	var options = {
+	  host: 'localhost',
+	  port: 3000,
+	  path: '/dummyJSON/recursos.juegos.json',
+	  method: 'GET'
+	};
+	http.request(options, function(response) {
+		var responseString = '';
+
+		response.on('data', function(data) {
+			responseString += data;
+		});
+
+		response.on('end', function() {
+			console.log(responseString);
+			var responseObject = JSON.parse(responseString);
+			res.send(responseObject);
+		});
+	}).end();
+});
+
+app.get('/videos', function(req, res){
+	var options = {
+	  host: 'localhost',
+	  port: 3000,
+	  path: '/dummyJSON/recursos.videos.json',
+	  method: 'GET'
+	};
+	http.request(options, function(response) {
+		var responseString = '';
+
+		response.on('data', function(data) {
+			responseString += data;
+		});
+
+		response.on('end', function() {
+			console.log(responseString);
+			var responseObject = JSON.parse(responseString);
+			res.send(responseObject);
+		});
+	}).end();
+});
+
+app.get('/infografias', function(req, res){
+	var options = {
+	  host: 'localhost',
+	  port: 3000,
+	  path: '/dummyJSON/recursos.infografias.json',
+	  method: 'GET'
+	};
+	http.request(options, function(response) {
+		var responseString = '';
+
+		response.on('data', function(data) {
+			responseString += data;
+		});
+
+		response.on('end', function() {
+			console.log(responseString);
+			var responseObject = JSON.parse(responseString);
+			res.send(responseObject);
+		});
+	}).end();
+});
+
+app.listen(app.get('port'));
+console.log('Server listening on port ' + app.get('port'));
