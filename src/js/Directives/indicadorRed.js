a6d1cec794aaf72451c6809ac3e2c562e5f4adbf
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-08 14:06:10 -0300

    Reparando las imágenes que no se mostraban en modo offline.

--- src/js/Directives/indicadorRed.js
+++ src/js/Directives/indicadorRed.js
@@ -3,7 +3,7 @@ var app = angular.module('app');
 app.directive('indicadorRed', function($http){
   return {
     restrict: 'E',
-    template: "<a id='connection-status-btn' class='btn btn-sm btn-default' ng-class='data.red.class'>{{data.red.texto}}</a>",
+    template: "<a id='connection-status-btn' class='btn btn-sm btn-default pull-right indicador' ng-class='data.red.class'>{{data.red.texto}}</a>",
     controller: function($scope, RedFactory){
       $scope.data = {};
       $scope.data.red = RedFactory;
