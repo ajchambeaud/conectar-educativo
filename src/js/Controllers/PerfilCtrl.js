0ac5963f5995beafd4df461510647bb6fa054bfa
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-08 15:42:41 -0300

    Implementando el guardado de preferencias de perfil.

--- src/js/Controllers/PerfilCtrl.js
+++ src/js/Controllers/PerfilCtrl.js
@@ -1,4 +1,21 @@
 var app = angular.module('app');
 
-app.controller('PerfilController', function(){
+app.controller('PerfilController', function($scope, PerfilFactory) {
+
+  $scope.data = {};
+
+  /* Se utiliza para deshabilitar el botón guardar. */
+  $scope.data.sin_cambios = false;
+  $scope.data.perfil = PerfilFactory.perfil;
+
+  /* Busca re-habilitar el botón guardar si se produce algún cambio. */
+  $scope.$watch('data.perfil.nombre', function(new_val, old_val) {
+    $scope.data.sin_cambios = false;
+  });
+
+  $scope.guardar = function() {
+    PerfilFactory.guardar($scope.data.perfil);
+    $scope.data.sin_cambios = true;
+  }
+
 });
