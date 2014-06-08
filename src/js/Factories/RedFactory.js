0f8390ec1a1fb7dfa4b0f64b0f68d7f3d49a971c
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-08 15:54:35 -0300

    Mejorando la verificación de acceso a internet (y haciendo que sea periódica).

--- src/js/Factories/RedFactory.js
+++ src/js/Factories/RedFactory.js
@@ -1,18 +1,38 @@
 var app = angular.module('app');
+var dns = require('dns');
 
 app.factory("RedFactory", function($timeout) {
   var obj = {};
+  obj.texto = "...";
 
-  obj.online = true;
-  obj.class = "btn-success";
-  obj.texto = "online";
+  function definir_modo_online() {
+    obj.online = true;
+    obj.class = "btn-success";
+    obj.texto = "online";
+  }
 
-  $timeout(function() {
+  function definir_modo_offline() {
     obj.online = false;
     obj.class = "btn-danger";
     obj.texto = "offline";
-    console.log("false!");
-  }, 2000);
+  }
+
+  function consultar_acceso_a_internet() {
+    console.log("Verificando acceso a intenet ...");
+
+    dns.resolve('http://www.google.com', function(err) {
+      $timeout(function() {
+        if (err)
+          definir_modo_offline();
+        else
+          definir_modo_online();
+      }, 10);
+    });
+
+    $timeout(consultar_acceso_a_internet, 5000);
+  }
+
+  consultar_acceso_a_internet();
 
   return obj;
 });
