74eaee0fc3c611eeef98d9c862b219ab6659fcce
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-13 14:45:15 -0300

    Creando DescargasFactory

--- src/js/Controllers/MisRecursosCtrl.js
+++ src/js/Controllers/MisRecursosCtrl.js
@@ -1,10 +1,10 @@
 var app = angular.module('app');
 
-app.controller('MisRecursosController', ['$http', function($http){
+app.controller('MisRecursosController', function($http, DescargasFactory){
   var misRecursos = this;
   misRecursos.recursos = null;
   $http.get('/recursos').success(function(data){
     misRecursos.recursos = data;
     console.log(data);
   });
-}]);
+});
---
+++ src/js/Factories/DescargasFactory.js
@@ -0,0 +1,7 @@
+var app = angular.module('app');
+
+app.factory("DescargasFactory", function() {
+  var obj = {};
+
+  return obj;
+});
