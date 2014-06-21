ae26444b994d10283d504fe512c9eb2cff0daf46
ajchambeaud <ajchambeaud@gmail.com>
2014-06-20 23:07:12 -0300

    se agrega librería underscore.js como modulo de angular

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('app', ['ngRoute', 'ngAnimate', 'angularSpinner', 'ui.bootstrap']);
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'angularSpinner', 'ui.bootstrap', 'underscore']);
---
+++ src/js/underscore.js
@@ -0,0 +1,5 @@
+var underscore = angular.module('underscore', []);
+
+underscore.factory('_', function() {
+		return window._; // assumes underscore has already been loaded on the page
+});
