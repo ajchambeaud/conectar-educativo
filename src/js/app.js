d6c8d198f5d58cd4234230caffdb1dcb3ed27c5b
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-06-29 22:55:43 -0300

    Haciendo que se puedan disparar búsquedas pulsando ENTER sobre el input de búsqueda.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('app', ['ngRoute', 'ngAnimate', 'angularSpinner', 'ui.bootstrap', 'underscore', 'infinite-scroll']);
+
+function procesar_tecla(e, id_boton_buscar) {
+    if (e.keyCode == 13)  {
+        document.getElementById(id_boton_buscar).click();
+        return false;
+    }
+}
