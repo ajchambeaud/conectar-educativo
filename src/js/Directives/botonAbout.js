445213484d21fd82562871d50125b2e388fb2b72
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-07-17 12:00:35 -0300

    Creando el dialogo "about"

---
+++ src/js/Directives/botonAbout.js
@@ -0,0 +1,28 @@
+var app = angular.module('app');
+
+app.directive('botonAbout', function($http, $modal){
+
+  return {
+    restrict: 'E',
+    template: "<a href ng-click='mostrar_dialogo_about()' class='boton-about'><i class='glyphicon glyphicon-question-sign'></i></a>",
+    controller: function($scope){
+      $scope.data = {};
+      $scope.mostrar_dialogo_about = function() {
+
+      var ModalAboutCtrl = function ($scope, $modalInstance) {
+        $scope.data = {};
+
+        $scope.ok = function () {
+          $modalInstance.close();
+        };
+      };
+
+      var modalInstance = $modal.open({
+        templateUrl: 'templates/modal_about.html',
+        controller: ModalAboutCtrl,
+      });
+
+      }
+    }
+  };
+});
