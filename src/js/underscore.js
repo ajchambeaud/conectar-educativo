b85583edb874fe5fe7f9071f889634a90ca8e38d
Ainu Jorge Chambeaud Helguera <ajchambeaud@gmail.com>
2015-03-10 11:41:02 -0300

    Update underscore.js

--- src/js/underscore.js
+++ src/js/underscore.js
@@ -1,5 +1,5 @@
 var underscore = angular.module('underscore', []);
 
-underscore.factory('_', function() {
-		return window._; // assumes underscore has already been loaded on the page
+underscore.factory('_', function($window) {
+		return $window._; // assumes underscore has already been loaded on the page
 });
