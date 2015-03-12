ff5ffc38a03cf3155c3c7b07c91649039306e620
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2015-03-12 15:14:26 -0300

    Migrado de metadata y contendios

--- scripts/migrador.py
+++ scripts/migrador.py
@@ -3,56 +3,75 @@
 
 import glob
 import json
+import os
+import os.path
+import shutil
 import xml.etree.ElementTree as ET
 
 
-def leer_xml(ruta):
+def leer_xml(ruta, tipo_funcional):
     tree = ET.parse(ruta)
     root = tree.getroot()
 
-    data = {
-        'id': int(root.find('id').text),
-        'titulo': root.find('titulo').text.strip(),
-        'descripcion': root.find('descripcion').text.strip(),
+    if int(root.find('tipo_funcional').attrib['id']) == tipo_funcional:
 
-        #debug
-        'tipo_funcional': (
-            int(root.find('tipo_funcional').attrib['id']),
-            root.find('tipo_funcional').attrib['value'],
-        ),
-        'tipo_rec_educativo': (
-            int(root.find('tipo_rec_educativo').attrib['id']),
-            root.find('tipo_rec_educativo').attrib['desc']
-        ),
-    }
+        data = {
+            'id': int(root.find('id').text),
+            'titulo': root.find('titulo').text.strip(),
+            'descripcion': root.find('descripcion').text.strip(),
 
-    return data
+            #debug
+            'tipo_funcional': (
+                int(root.find('tipo_funcional').attrib['id']),
+                root.find('tipo_funcional').attrib['value'],
+            ),
+            'tipo_rec_educativo': (
+                int(root.find('tipo_rec_educativo').attrib['id']),
+                root.find('tipo_rec_educativo').attrib['desc']
+            ),
+        }
+
+        #copiar archivos
+        try:
+            data['screenshot'] = root.find('screenshots').findall('screenshot')[0].attrib['file']
+        except:
+            data['screenshot'] = ''
+
+        data['archivo'] = root.find('tipo_funcional').find('video').find('file').text
+
+
+        return data
+
+    return False
 
 
 RECURSOS_ORIGEN = '/home/alumno/MiEscritorio/repositorio/'
-RECURSOS_DESTINO = ''
+RECURSOS_DESTINO = '/home/alumno/ConectarEducativo'
+
 EDUCAR_URL = 'http://www.educ.ar/sitios/educar/recursos/ver?id='
 
 recursos = glob.glob('%s/*' % RECURSOS_ORIGEN)
 
 ce_recursos = {'recursos': []}
 
+"""
+tipo_funcional:
+    ('1', 'Unidad HTML')
+    ('2', 'Video')
+    ('7', 'Flash')
+
+tipo_rec_educativo:
+    (2, 'Video')
+    (5, u'Infografía')
+    (10, 'Actividades')
+"""
+
+fallaos = 0
+
 for recurso in recursos:
-    data = leer_xml('%s/info.xml' % recurso)
+    data = leer_xml('%s/info.xml' % recurso, tipo_funcional=2)
 
-    """
-    tipo_funcional:
-        ('1', 'Unidad HTML')
-        ('2', 'Video')
-        ('7', 'Flash')
-
-    tipo_rec_educativo:
-        (2, 'Video')
-        (5, u'Infografía')
-        (10, 'Actividades')
-    """
-
-    if data['tipo_funcional'][0] == 2:
+    if data:
         ce_data = {
             'id': data['id'],
             'titulo': data['titulo'],
@@ -60,6 +79,24 @@ for recurso in recursos:
             'url': '%s%s' % (EDUCAR_URL, data['id']),
             'entity': 'video'
         }
+
+        os.makedirs(os.path.join(RECURSOS_DESTINO, str(data['id'])))
+
+        try:
+            shutil.copy(
+                os.path.join(RECURSOS_ORIGEN, data['screenshot']),
+                os.path.join(RECURSOS_DESTINO, str(data['id']), 'thumb.jpg')
+            )
+        except:
+            pass
+        try:
+            shutil.copy(
+                os.path.join(RECURSOS_ORIGEN, data['archivo']),
+                os.path.join(RECURSOS_DESTINO, str(data['id']), 'video')
+            )
+        except:
+            print os.path.join(RECURSOS_ORIGEN, data['archivo'])
+            print "\n"
 
         ce_recursos['recursos'].append(ce_data)
 
